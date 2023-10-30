2bd870c7b0a831118a1919fabe22850cff38eb11
Kristofer <145542649+kiku69@users.noreply.github.com>
2023-10-30 13:24:25 +0200

    Create yl10.py

---
+++ yl10.py
@@ -0,0 +1,14 @@
+ame = input("palun sisestage oma nimi: ")
+print("tere", name)
+
+area = input("palun sisestage elukoht: ")
+if "saaremaa" in area.lower():
+    print("tere saarlane")
+
+age = input("palun sisestage oma vanus: ")
+if age < 17:
+    print("sa oled liiga noor")
+elif age == 18:
+    print("palju õnne, sa oled täisealine!")
+else:
+    print("sa võid autot juhtida")

693f1123aabb62424fe1e33e28dd31b3b3ccfe96
Kristofer <145542649+kiku69@users.noreply.github.com>
2023-11-02 10:56:41 +0200

    Update yl13.py

--- yl13.py
+++ yl13.py
@@ -1,6 +1,12 @@
-pet = input("palun sisesta lemmikloom")
-print(pet[0])
+pet = input("Sisesta oma lemmikloom: ")
 
-petlist = ["cat", "dog", "cow"]
-petlist.append(pet)
-print(petlist)
+print("first letter:", pet[0])
+
+pet_list = ["cat", "dog", "cow"]
+
+pet_list.append(pet)
+
+print("pet_list:", pet_list)
+
+Last_letter = pet_list[-1][-1]
+print("Last_letter:", Last_letter)

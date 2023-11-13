f7799cdf5c0e71e4cb34c6e05f42b9370892a998
Kristofer <kristofer.mere@ametikool.ee>
2023-11-13 13:02:05 +0200

    yl 10, 12 ,13

---
+++ yl12.py
@@ -0,0 +1,13 @@
+fruitlist = ["apple", "kiwi", "cherry"]
+print(fruitlist[0])
+fruitlist.append("pear")
+print(fruitlist)
+print(fruitlist[-1])
+fruitlist[1]= "blaxkcurrat"
+print(fruitlist)
+if "pinapple" in fruitlist:
+    print("apple is on the list")
+
+print(len(fruitlist))
+fruitlist.pop(1)
+print(fruitlist)

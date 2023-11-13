b6c68de458f3932d279c532b8e9526fed5016ad8
Kristofer <145542649+kiku69@users.noreply.github.com>
2023-11-13 13:55:20 +0200

    Update yl19.py

--- yl19.py
+++ yl19.py
@@ -1,4 +1,11 @@
 letters = ("a", "e", "i", "o", "u", "õ", "ä", "ö", "ü")
-a = input("sisesta sõna")
+a = input("sisesta sõna").lower()
+
 b=a.count("a") +a.count("e") +a.count("i") +a.count("o") +a.count("u") +a.count("õ") +a.count("ä") +a.count("ö") +a.count("ü")
 print(b)
+
+i = 0
+for c in a:
+    if c in letters:
+        i += 1
+print(i)

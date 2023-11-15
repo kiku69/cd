437948eed81261db2e7bc2e86bff0ed6ecd4133a
Kristofer <145542649+kiku69@users.noreply.github.com>
2023-11-15 13:06:53 +0200

    Update yl22.py

--- yl22.py
+++ yl22.py
@@ -8,9 +8,10 @@ while True:
     if a == "x":
         break
 
-    if a == "kivi" and r == "käärid" or a == "paber" and r == "kivi" or a == "käärid" and r == "paber" or a == "kivi" and r == "kivi"
+    if a == "kivi" and r == "käärid" or a == "paber" and r == "kivi" or a == "käärid" and r == "paber" or a == "kivi" and r == "paber" or a == "käärid" and r == "kivi" or a == "paber" and r == "käärid":
         print("sinu võit")
-    elif a == "kivi" and r == "paber" or a == "paber" and r == "käärid" or a == "käärid" and r == "paber"
+    elif a == "kivi" and r == "paber" or a == "paber" and r == "käärid" or a == "käärid" and r == "paber" or a == "paber" and r == "kivi" or a == "kivi" and r== "käärid" or r == "käärid" and r =="kivi":
         print("sinu kaotus")
     else:
         print("viik")
+
